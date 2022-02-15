b1e1f22bae92ee6c026ff51aa554cf304cb6f107
Brenojbs <brenojbs@hotmail.com>
2022-02-15 13:51:13 -0300

    Criando pasta src e arquivo index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const { userController, loginController } = require('./controllers');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/user', userController);
+app.post('/login', loginController);
+
+app.listen(PORT, () => {
+  console.log(`App ouvindo porta ${PORT}`);
+});
